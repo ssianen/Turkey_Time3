97f02f7d819c257392f56a467f7dfe7a376edb60
Matt <153225699+ScivenMatt@users.noreply.github.com>
2024-10-23 10:37:19 -0700

    Feat: The player can now do damage
    Apples now actually do damage and you can see the enemy health bars decrease before they disappear

--- Assets/Scripts/ProjectileApple.cs
+++ Assets/Scripts/ProjectileApple.cs
@@ -6,7 +6,19 @@ public class ProjectileApple : MonoBehaviour
 {
     // Start is called before the first frame update
     public Vector2 velocity = new Vector2(0.0f,0.0f);
+    public int damage = 10;
     public GameObject turkey;
+    public GameObject cowNPC;
+    EnemyHealth cowHealth;
+    public GameObject horseNPC;
+    EnemyHealth horseHealth;
+    public GameObject pigNPC;
+    EnemyHealth pigHealth;
+    public GameObject chickenNPC;
+    EnemyHealth chickenHealth;
+    public GameObject slimeNPC;
+    EnemyHealth slimeHealth;
+
     void Update() {
         Vector2 currentPosition = new Vector2(transform.position.x, transform.position.y);
         Vector2 newPosition = currentPosition + velocity * Time.deltaTime;
@@ -18,7 +30,38 @@ public class ProjectileApple : MonoBehaviour
             GameObject other = hit.collider.gameObject;
             if (other != turkey)
             {
-                Debug.Log(hit.collider.gameObject);
+                //Debug.Log(hit.collider.gameObject);
+                if (other == cowNPC)
+                {
+                    cowHealth = cowNPC.GetComponent<EnemyHealth>();
+                    Debug.Log("Ouch!\n");
+                    cowHealth.TakeDamage(damage);
+                    Destroy(gameObject);
+                }
+                else if (other == horseNPC)
+                {
+                    horseHealth = horseNPC.GetComponent<EnemyHealth>();
+                    horseHealth.TakeDamage(damage);
+                    Destroy(gameObject);
+                }
+                else if (other == pigNPC)
+                {
+                    pigHealth = pigNPC.GetComponent<EnemyHealth>();
+                    pigHealth.TakeDamage(damage);
+                    Destroy(gameObject);
+                }
+                else if (other == chickenNPC)
+                {
+                    chickenHealth = chickenNPC.GetComponent<EnemyHealth>();
+                    chickenHealth.TakeDamage(damage);
+                    Destroy(gameObject);
+                }
+                else if (other == slimeNPC)
+                {
+                    slimeHealth = slimeNPC.GetComponent<EnemyHealth>();
+                    slimeHealth.TakeDamage(damage);
+                    Destroy(gameObject);
+                }
             }
         }
 

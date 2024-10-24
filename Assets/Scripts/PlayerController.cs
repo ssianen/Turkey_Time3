bdd1ea99401d5261e83c4f6f143931f5463c736d
ssianen <92133110+ssianen@users.noreply.github.com>
2024-10-24 11:44:08 -0700

    fix: compiler error preventing build

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEditor.UIElements;
+using UnityEngine.UI;
 using UnityEngine.Animations;
 
 public class PlayerController : MonoBehaviour
